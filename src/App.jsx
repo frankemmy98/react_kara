cd792ea463bdd0901cb387c6b84e796ab6a5c292
Emmanuel Frank <emmyfrank349@gmail.com>
2025-06-21 16:26:58 +0100

    feat: installation of react vite, tailwind.css

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-indigo-500 to-purple-600 text-white">
+      <h1 className="text-5xl font-extrabold mb-4">🎶 Welcome to React-Kara</h1>
+      <p className="text-lg">Powered by React, Vite & Tailwind CSS v3</p>
+    </div>
+  );
+}
+
+export default App;
